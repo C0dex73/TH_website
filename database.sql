15d0493b41baaa67d36c8bfd8588e42dad35623b
C0dex73 <c0d3x73@gmail.com>
2023-10-02 21:38:05 +0200

    updated database.sql

--- database.sql
+++ database.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1
--- Généré le : dim. 01 oct. 2023 à 16:58
--- Version du serveur : 10.4.28-MariaDB
--- Version de PHP : 8.2.4
+-- Host : 127.0.0.1
+-- Generated in : Mon. 02 oct. 2023 at 21:32
+-- server version : 10.4.28-MariaDB
+-- PHP version : 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,34 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `th_internal`
+-- Data Base : `th_internal`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `login`
+-- `blog` table structure
+--
+
+CREATE TABLE `blog` (
+  `author` text NOT NULL DEFAULT '[SYSTÈME]',
+  `title` text NOT NULL DEFAULT '[Sans titre]',
+  `content` text NOT NULL DEFAULT '[Sans contenu]',
+  `files` text NOT NULL,
+  `published` datetime NOT NULL DEFAULT '2007-08-05 03:47:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Setting values of `blog`
+--
+
+INSERT INTO `blog` (`author`, `title`, `content`, `files`, `published`) VALUES
+('[SYSTEM]', 'Test title', 'Default content', '', '2023-10-02 21:36:00');
+
+-- --------------------------------------------------------
+
+--
+-- `login` table structure
 --
 
 CREATE TABLE `login` (
@@ -37,11 +58,11 @@ CREATE TABLE `login` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Déchargement des données de la table `login`
+-- Setting values of `login`
 --
 
 INSERT INTO `login` (`username`, `password`, `token`, `email`, `Vcode`, `VcodeEx`) VALUES
-('example_username', 'example_password', 'example_token', 'example@email.com', '3xampl3Vc0d3', '2007-08-05 03:47:00');
+('admin', 'admin', '16651a8c25ed881', 'admin@localhost.com', '-1', '2023-10-03 11:23:49');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
