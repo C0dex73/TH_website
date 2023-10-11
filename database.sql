2daec01d30e146ee3fceb8891714dfaee1cfef64
C0dex73 <c0d3x73@gmail.com>
2023-10-11 18:14:54 +0200

    sql update

--- database.sql
+++ database.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host : 127.0.0.1
--- Generated in : Mon. 02 oct. 2023 at 21:32
--- server version : 10.4.28-MariaDB
--- PHP version : 8.2.4
+-- Hôte : 127.0.0.1
+-- Généré le : mer. 11 oct. 2023 à 17:51
+-- Version du serveur : 10.4.28-MariaDB
+-- Version de PHP : 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,34 +18,28 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Data Base : `th_internal`
+-- Base de données : `th_internal`
 --
 
 -- --------------------------------------------------------
 
 --
--- `blog` table structure
+-- Structure de la table `blog`
 --
 
 CREATE TABLE `blog` (
+  `id` int(11) NOT NULL,
   `author` text NOT NULL DEFAULT '[SYSTÈME]',
   `title` text NOT NULL DEFAULT '[Sans titre]',
   `content` text NOT NULL DEFAULT '[Sans contenu]',
   `files` text NOT NULL,
-  `published` datetime NOT NULL DEFAULT '2007-08-05 03:47:00'
+  `published` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Setting values of `blog`
---
-
-INSERT INTO `blog` (`author`, `title`, `content`, `files`, `published`) VALUES
-('[SYSTEM]', 'Test title', 'Default content', '', '2023-10-02 21:36:00');
 
 -- --------------------------------------------------------
 
 --
--- `login` table structure
+-- Structure de la table `login`
 --
 
 CREATE TABLE `login` (
@@ -58,11 +52,24 @@ CREATE TABLE `login` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Setting values of `login`
+-- Index pour les tables déchargées
 --
 
-INSERT INTO `login` (`username`, `password`, `token`, `email`, `Vcode`, `VcodeEx`) VALUES
-('admin', 'admin', '16651a8c25ed881', 'admin@localhost.com', '-1', '2023-10-03 11:23:49');
+--
+-- Index pour la table `blog`
+--
+ALTER TABLE `blog`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `blog`
+--
+ALTER TABLE `blog`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
