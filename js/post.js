7b2d4a6989bf7656af18885dfefc083c7c3ab769
Codex73 <95096929+C0dex73@users.noreply.github.com>
2024-02-01 20:11:25 +0100

    Fixed bugged post
    It didn't update the contentvalue input

--- js/post.js
+++ js/post.js
@@ -114,6 +114,7 @@ $(() => {
     function Render() {
         txtAera.value = bred(txtAera.value);
         $('#render').html(txtAera.value);
+        $('contentvalue').html(txtAera.value);
         multiImgPreview($('#file'), '#render');
     }
 
